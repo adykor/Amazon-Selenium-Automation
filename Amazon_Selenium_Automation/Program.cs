49f934615a6b112c83c4af2520cb05118a27b221
kyerstin-oneal <116093833+kyerstin-oneal@users.noreply.github.com>
2022-10-20 10:12:45 -0600

    Initial project commit

---
+++ Amazon_Selenium_Automation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_Automation
+{
+    class Program
+    {
+
+        // Create a reference for Chrome browser
+        IWebDriver driver = new ChromeDriver();
+
+        //static void Main(string[] args)
+        //{
+        //}
+
+        [SetUp]
+        public void Initialize()
+        {
+            // Go to Amazon page
+            driver.Navigate().GoToUrl("https://www.amazon.com");
+        }
+
+
+        [Test]
+        public void ExecuteTest()
+        {
+            // Make the browser full screen
+            driver.Manage().Window.Maximize();
+
+            // Web elements & their respective operations
+            IWebElement SignIn = driver.FindElement(By.Id("nav-link-accountList"));
+            SignIn.Click();
+
+            IWebElement EmailField = driver.FindElement(By.Id("ap_email"));
+            EmailField.SendKeys("pewpew@abv.bg");
+
+            IWebElement ContinueButton = driver.FindElement(By.Id("continue"));
+            ContinueButton.Click();
+
+            IWebElement ErrorMessage = driver.FindElement(By.ClassName("a-alert-heading"));
+
+            // Assertion variables
+            string ActualErrorMessageText = ErrorMessage.Text;
+            string ExpectedErrorMessageText = "There was a problem";
+
+            // Assertion
+            Assert.AreEqual(ActualErrorMessageText, ExpectedErrorMessageText);
+        }
+
+
+        [TearDown]
+        public void CloseTest()
+        {
+            // Close the browswer
+            driver.Quit();
+        }
+
+    }
+}
+
